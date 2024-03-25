774c128dbf87b761b75cad3bfff34b02eae2faa3
Diego Motta <dmotta@farmakd.com>
2024-03-25 18:39:06 -0300

    Added /api/executed/create route, created controller to store data executed from cron, and, created a commando to executed jobs and scheduled cron every 2 minutes

---
+++ app/Console/Commands/ExecuteTotal.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Executed;
+use App\Models\Order;
+use App\Models\OrderLine;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class ExecuteTotal extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'execute:total';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Calculate total cost of all orders';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+
+       /*
+        Calcular de forma asíncrona el coste total de todos los pedidos de la DB. 
+        Para calcular este coste hay que multiplicar cada order_line “qty” por el “product cost” (colocar un nombre a la queue). 
+        Una vez sumados todos los pedidos guardar el resultado en la tabla executed pegando al endpoint /api/executed/create
+        */
+
+        // Calcular el costo total de todos los pedidos
+        $totalCost = OrderLine::with('product')->get()->sum(function ($orderLine) {
+            return $orderLine->qty * $orderLine->product->cost;
+        });
+
+        //No se especifica que ordenes ya se ejecutaron en el challege considero que todas ordenes
+
+ 
+        $totalOrders = Order::count();
+        // Llamar al endpoint /api/executed/create
+        $response = $this->call('POST', '/api/executed/create', ['total_cost' => $totalCost, 'total_orders' => $totalOrders]);
+
+        $this->info('Total cost calculated and saved successfully.');
+    }
+}
---
+++ app/Http/Controllers/ExecutedController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Executed;
+
+class ExecutedController extends Controller
+{
+    //
+    public function store(Request $request)
+    {
+        // Aquí guardas los datos recibidos en la tabla "executed"
+        // Puedes usar el método create si tienes los campos masivos definidos en el modelo
+        Executed::create($request->all());
+
+        return response()->json(['message' => 'Data saved successfully'], 200);
+    }
+}
